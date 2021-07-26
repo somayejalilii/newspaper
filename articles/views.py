a6624aefdd9dcd34eaa4a79e435d17f9f24fe61c
somayejalilii <m62heidary@gmail.com>
2021-07-26 19:47:24 +0430

    first commit

---
+++ articles/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Article
+
+class ArticleListView(LoginRequiredMixin, ListView): # new
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(LoginRequiredMixin, DetailView): # new
+    model = Article
+    template_name = 'article_detail.html'
+
+class ArticleUpdateView(LoginRequiredMixin, UpdateView): # new
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView): # new
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body',)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
